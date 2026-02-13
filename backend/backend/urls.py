75365464ad8d7b75687060541cf1206a27e8fdcb
hibah <hibahmohammed.k@gmail.com>
2026-02-13 13:08:25 +0400

    resolved profile image display

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/adminpanel/', include('adminpanel.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
